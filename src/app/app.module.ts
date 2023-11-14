e0f61153e2c7f730f5a34f6d8de6259dff7e7fd7
Marcos lopez <marcosmatiaslopez.2018@gmail.com>
2023-11-13 23:34:24 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserModule } from './user/user.module';
+import { AdminModule } from './admin/admin.module';
+import { SharedModule } from './shared/shared.module';
+import { NgxSplideModule } from 'ngx-splide';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserModule,
+    AdminModule,
+    SharedModule,
+    NgxSplideModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user/components/home/home.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import Splide from '@splidejs/splide';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit, AfterViewInit {
+  ngAfterViewInit(): void {
+    new Splide('.splide', {
+      type: 'loop',
+      perPage: 1,
+      perMove: 1,
+      autoplay: true,
+      interval: 7000, // Adjust the interval as desired
+      focus: 'center', // Add this line to center the active slide
+      pagination: false,
+      mediaQuery: 'min',
+
+
+    }).mount();
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
